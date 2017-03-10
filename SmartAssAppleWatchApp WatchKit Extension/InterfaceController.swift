ebc5652376dbd14eeeef526f00ef2ca3563ec49e
melhoos <mmelhoos@gmail.com>
2017-03-10 18:15:36 +0100

    first commit

--- SmartAssAppleWatchApp WatchKit Extension/InterfaceController.swift
+++ SmartAssAppleWatchApp WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,9 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+
+    @IBOutlet var notifySlackButton: WKInterfaceButton!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -27,5 +30,37 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func ActionForButton() {
+        makePOSTcall()
+    }
+    
+    func makePOSTcall() {
+        
+        let json: [String: Any] = ["text": "test"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        var request = URLRequest(url: URL(string: "https://hooks.slack.com/services/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")!)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+        
+    }
+    
+    
 
 }
