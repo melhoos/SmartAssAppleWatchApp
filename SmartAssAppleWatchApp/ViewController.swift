887d45610d473348d40470d9f7ca4ac3ec9b885e
melhoos <mmelhoos@gmail.com>
2017-03-10 17:54:02 +0100

    Initial Commit

---
+++ SmartAssAppleWatchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartAssAppleWatchApp
+//
+//  Created by Marianne Melhoos on 10/03/17.
+//  Copyright © 2017 Marianne Melhoos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
